6f7c7bd1673cdf1d5cbcb101f956d90760876ad7
vineetkhadloya <Vonface@98>
2021-10-15 13:51:10 -0400

    commit on 'master'

---
+++ src/1008. Construct Binary Search Tree from Preorder Traversal.py
@@ -0,0 +1,45 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]:
+        
+        print("preorder = ",preorder)
+        
+        # If List is empty then return None ==> Leaf Node Child
+        if not preorder: return None
+        
+        root = TreeNode(preorder[0])
+        
+        print("root = ",root)
+        
+        # If List length is one then it is leaf Node
+        if len(preorder) == 1: return root
+        
+        # Calculate start of right Sub Tree
+        left = 1
+        right = len(preorder) - 1
+
+        while left < right:
+            print("left,right = ",left,right)
+            mid = (left + right) // 2
+            if(preorder[mid] < root.val):
+                left = mid + 1
+            else:
+                right = mid
+            
+        print("Final left,right = ",left,right)
+        
+        # If value of leftmost is less than value of root this indicates there is no right sub tree
+        if(preorder[left] < root.val):
+            print(preorder)
+            print("If")
+            root.left = self.bstFromPreorder(preorder[1:])
+        else:
+            root.left = self.bstFromPreorder(preorder[1:left])
+            root.right = self.bstFromPreorder(preorder[left:])
+        print("-------")
+        return root
